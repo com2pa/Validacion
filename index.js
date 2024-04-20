0be1c6e63b7be1b68b62a00df1f4fd9c14715ac8
merwil vegas <com2pa@gmail.com>
2024-04-20 16:37:58 -0400

    se añadio funcion de valicon de datos y seleccion de codigo de pais

--- index.js
+++ index.js
@@ -10,11 +10,35 @@ const countries = document.querySelector('#countries');
 // paso 2
 const usernameInput = document.querySelector('#username');
 const emailInput=document.querySelector('#email');
+const phoneCode = document.querySelector('#phone-code')
 
 // -- validaciones
 let usernameValidation = false;
+let emailValidation= false;
+// let countriesValidation=false;
 
+// paso3.1
+// funcion para validacion estados input
 
+const validacion=(e, validacion,element)=>{
+   
+    // const texto= usernameInput.parentElement.children[1]
+    const informacion= e.target.parentElement.children[1]
+   
+    if(validacion.value== ''){
+        element.classList.remove('correct')
+        element.classList.remove('incorrect')
+        informacion.classList.remove('show-informacion')
+    }else if(validacion){
+        element.classList.add('correct')
+        element.classList.remove('incorrect')
+        informacion.classList.remove('show-informacion')
+    }else{
+        element.classList.add('incorrect')
+        element.classList.remove('correct')
+        informacion.classList.add('show-informacion')
+    }
+}
 
 // paso 1 
 [... countries].forEach(option=>{
@@ -23,24 +47,18 @@ let usernameValidation = false;
 // paso 3
 // estado
 usernameInput.addEventListener('input',e =>{
-    // console.log(e.target.value);
-    
     usernameValidation= USERNAME_REGEX.test(e.target.value)
-    console.log(usernameValidation);
-    const texto= usernameInput.parentElement.children[1]
-    // const informacion= e.target.parentElement.children[1]
-    console.log(texto);
-    if(usernameValidation.value== ''){
-        usernameInput.classList.remove('correct')
-        usernameInput.classList.remove('incorrect')
-        texto.classList.remove('show-informacion')
-    }else if(usernameValidation){
-        usernameInput.classList.add('correct')
-        usernameInput.classList.remove('incorrect')
-        texto.classList.remove('show-informacion')
-    }else{
-        usernameInput.classList.add('incorrect')
-        usernameInput.classList.remove('correct')
-        texto.classList.add('show-informacion')
-    }
+    validacion(e,usernameValidation,usernameInput)//e= evento, usernameValidation =validacion ,usernameInput = input
+    
 })
+
+emailInput.addEventListener('input',e =>{
+    emailValidation= EMAIL_REGEX.test(e.target.value)
+    validacion(e,emailValidation,emailInput)
+})
+// paso3.2 evento para seleccionar el pais y codigo de telf 
+countries.addEventListener('input',e =>{
+    const optionSelected =[... e.target.children].find(option => option.selected);
+    phoneCode.innerHTML=`+${optionSelected.value}`
+})
+
